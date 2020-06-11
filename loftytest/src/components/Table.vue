d87ce638deb1537edce0b85e000f9c963a01b008
mikavanko <mikhailierakliy@mail.ru>
2020-06-11 13:33:30 +0300

    test

---
+++ loftytest/src/components/Table.vue
@@ -0,0 +1,92 @@
+<template>
+    <v-container fluid class="pl-0 pr-0">
+        <v-btn color="teal" dark class="mb-5 ml-5" @click="editRow">
+            <v-icon dark>mdi-plus</v-icon>
+            <span>Создать новый товар</span>
+        </v-btn>
+
+        <v-data-table
+            :headers="headers"
+            :items="getData"
+            :items-per-page="10"
+            class="elevation-1"
+        >
+            <template v-slot:item.controls="props">
+                <v-btn class="mr-2" icon small @click="editRow(props.item)">
+                    <v-icon dark>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn class="mr-2" icon small @click="deleteRow(props.item)">
+                    <v-icon dark>mdi-delete</v-icon>
+                </v-btn>
+            </template>
+        </v-data-table>
+    </v-container>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+    name: 'Table',
+    components: {
+    },
+    data: () => ({
+        headers: [
+            {
+                text: 'Артикул',
+                sortable: false,
+                value: 'artnumber',
+            },
+            {
+                text: 'Название товара',
+                value: 'name',
+            },
+            {
+                text: 'Бренд',
+                value: 'brand',
+            },
+            {
+                text: 'Вес',
+                value: 'weight',
+            },
+            {
+                text: 'Фасовка',
+                value: 'quantity',
+            },
+            {
+                text: 'Цена',
+                value: 'price',
+            },
+            {
+                text: 'Наличие',
+                value: 'stock',
+            },
+            {
+                text: "",
+                value: "controls",
+                sortable: false}
+        ]
+    }),
+    created() {
+        this.$store.dispatch('initData');
+    },
+    computed: {
+        ...mapGetters(['getData'])
+    },
+    methods: {
+        editRow(item){
+            const idx = this.getData.indexOf(item);
+
+            if(idx > -1){
+                this.$store.dispatch('setEditedIdx', idx);
+                this.$store.dispatch('setEditedRow', item);
+            }
+            this.$store.dispatch('togglePopup');
+        },
+        deleteRow(item){
+            const idx = this.getData.indexOf(item)
+            confirm('Вы уверены что хотите удалить этот товар?') && this.getData.splice(idx, 1)
+        }
+    }
+}
+</script>
--- loftytest/src/store/index.js
+++ loftytest/src/store/index.js
@@ -1,15 +1,93 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import dataRaw from "@/assets/data.json"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    data: null,
+    popup: false,
+    editedIdx: -1,
+    editedRow: {
+      artnumber: 0,
+      name: '',
+      brand: '',
+      weight: 0,
+      quantity: 0,
+      price: 0,
+      stock: 0
+    },
+    defaultRow: {
+      artnumber: 0,
+      name: '',
+      brand: '',
+      weight: 0,
+      quantity: 0,
+      price: 0,
+      stock: 0
+    }
   },
   mutations: {
+    initData(state, payload) {
+      state.data = payload
+    },
+    togglePopup(state) {
+      state.popup = !state.popup
+    },
+    setEditedIdx(state, idx) {
+      state.editedIdx = idx
+    },
+    setEditedRow(state, item) {
+      console.log(item)
+      state.editedRow = Object.assign({}, item)
+    },
+    setData(state) {
+      if (state.editedIdx > -1) {
+        Object.assign(state.data[state.editedIdx], state.editedRow)
+      } else {
+        state.editedRow.artnumber = Math.random().toString().substr(2, 7);
+        state.data.unshift(state.editedRow)
+      }
+    },
+    resetToDefaultRow(state) {
+      state.editedRow = Object.assign({}, state.defaultRow)
+      state.editedIdx = -1
+    }
   },
   actions: {
+    initData(ctx) {
+      let data = dataRaw;
+      ctx.commit('initData', data)
+    },
+    togglePopup(ctx) {
+      ctx.commit('togglePopup')
+    },
+    setEditedIdx(ctx, idx) {
+      ctx.commit('setEditedIdx', idx)
+    },
+    setEditedRow(ctx, item) {
+      ctx.commit('setEditedRow', item)
+    },
+    setData(ctx) {
+      ctx.commit('setData')
+    },
+    resetToDefaultRow(ctx) {
+      ctx.commit('resetToDefaultRow')
+    }
   },
-  modules: {
+  getters: {
+    getData: state => {
+      return state.data
+    },
+    getPopup: state => {
+      return state.popup
+    },
+    getEditedRow: state => {
+      return state.editedRow
+    },
+    getEditedIdx: state => {
+      return state.editedIdx
+    }
   }
 })

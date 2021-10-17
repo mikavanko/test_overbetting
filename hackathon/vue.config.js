aa9324353ae50ea51e562a90a2a5250c91dfa094
mikhailivanko <mikhailivanko@gmail.com>
2021-10-17 22:43:05 +0300

    fix

--- hackathon/vue.config.js
+++ hackathon/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/hackathon/dist/' : '/hackathon/dist/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/hackathon/dist/' : '/'
 }

41a7ca1e4e134c0f3a08d2fa025a81bd5ed54137
mikhailivanko <mikhailivanko@gmail.com>
2021-11-06 15:36:00 +0300

    player fix

--- hackathon/src/pages/Player.vue
+++ hackathon/src/pages/Player.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="edit-page">
     <div class="wrapper">
-      <template v-if="file && time">
+      <template v-if="!hasError">
         <h2 class="h2">{{title}}</h2>
         
-        <div v-if="audioUrl"
-            class="player">
+        <div class="player">
           <audio
               ref="player"
               controls
@@ -13,6 +12,26 @@
                   Your browser does not support the
                   <code>audio</code> element.
           </audio>
+        </div>
+
+        <h3 class="h3">Проигрываемый текст:</h3>
+        <div class="text-block">
+          <textarea class="textarea" v-model="text" />
+          <div class="text-block__item">
+            <button v-if="!isPlaying"
+                    class="btn"
+                    @click="play">Проиграть</button>
+            <button v-else
+                    class="btn"
+                    @click="pause">Стоп</button>
+            <button v-if="!copyDone"
+                    class="btn"
+                    @click="copy">Копировать текст</button>
+            <button v-else
+                    class="btn btn_copy"
+                    disabled
+                    @click="copy">Скопировано</button>
+          </div>
         </div>
       </template>
       <template v-else>
@@ -22,7 +41,7 @@
           <br>
           <br>
           <br>
-          <h2 class="h2">Неверный url</h2>
+          <h2 class="h2">{{errorText}}</h2>
         </center>
       </template>
     </div>
@@ -30,43 +49,135 @@
 </template>
 
 <script>
+let clearId = null
+
 export default {
   name: 'PlayerPage',
   data() {
     return {
-      isPlay: false,
-      item: null,
       file: null,
-      time: null,
+      start: null,
+      end: null,
+      text: null,
+      period: null,
+      player: null,
+      isPlaying: false,
+      copyDone: false,
+      errorText: 'Неверный url',
+      hasError: false
     }
   },
   mounted() {
     this.file = this.$route.query.file
-    this.time = parseInt(this.$route.query.t)
-    const _this = this
+    this.text = this.$route.query.text
+    this.start = parseInt(this.$route.query.start)
+    this.end = parseInt(this.$route.query.end)
+    
+    this.hasError = !this.file || !this.text || !this.start || !this.end
 
-    setTimeout(function(){
-      _this.$refs.player.currentTime = _this.time
-      // _this.$refs.player.play()
-    }, 300)
+    if(!this.hasError) {
+      this.period = +(this.end - this.start).toFixed(2)
+
+      this.player = this.$refs.player
+      this.player.currentTime = this.start
+      
+      this.player.onplay = (event) => {
+        this.isPlaying = true
+        if(this.period > 0)
+        clearId = setTimeout(() => {
+          this.player.pause()
+        }, this.period * 1000)
+      }
+      this.player.onpause = (event) => {
+        this.isPlaying = false
+        this.player.currentTime = this.start
+        clearInterval(clearId)
+      }
+    }
+  },
+  methods: {
+    play() {
+      this.player && this.player.play()
+    },
+    pause() {
+      this.player && this.player.pause()
+    },
+    copy() {
+      var copyTextarea = document.querySelector('.textarea')
+      copyTextarea.focus()
+      copyTextarea.select()
+
+      try {
+        this.copyDone = document.execCommand('copy')
+        if(this.copyDone){
+          setTimeout(()=>{
+            this.copyDone = false
+          }, 1000)
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
   },
   computed: {
     title() {
       return this.file?.replace(/\.[^/.]+$/, "") ?? ''
     },
-    audioUrl: {
-      get(){
-        return this.file ? `https://storage.googleapis.com/hakathon/${this.file}` : null
-      }
-    },
-    content() {
-      return this.item?.data ?? []
+    audioUrl() {
+      return this.file ? `https://storage.googleapis.com/hakathon/${this.file}` : null
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
+.text-block{
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  &__item{
+    margin-left: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+}
+.btn{
+  @include clear-btn();
+
+  height: 30px;
+  border-radius: 5px;
+  background-color: $body-color;
+  color: $main-color;
+  border: 1px solid $main-color;
+  padding: 0 15px;
+  margin-bottom: 15px;
+
+  &_copy{
+    color: rgba(green, .5)
+  }
+
+  &:not([disabled]):hover{
+    background-color: $hover-color;
+    color: $body-color;
+    border-color: $hover-color;
+  }
+}
+.edit-page{
+  font-family: $font-family;
+}
+.textarea{
+  font-family: $font-family;
+  font-size: 14px;
+  width: 300px;
+  height: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+}
 .player{
   audio{
     width: 100%;
@@ -76,6 +187,13 @@ export default {
   font-weight: 500;
   font-size: 24px;
   line-height: 29px;
+  color: $body-color;
+  margin-bottom: 13px;
+}
+.h3{
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
   color: $body-color;
   margin-bottom: 13px;
 }

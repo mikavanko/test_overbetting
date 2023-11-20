c68a040111c923441fe7a8d5551d2451ddc6f454
mikavanko <mikhailivanko@gmail.com>
2023-11-21 00:09:14 +0300

    fix base and build

--- front-task/vite.config.ts
+++ front-task/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'front-task',
+  base: process.env.NODE_ENV === 'production' ? '/front-task/dist/' : '/front-task',
   plugins: [
     vue(),
   ],
